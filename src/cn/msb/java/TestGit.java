b2a983586e2a60899955a3b992a3cf8a9f0a693f
Harvey <harvey9607@1643.com>
2020-11-14 16:53:39 +0800

    作为测试第一次提交到远程仓库

---
+++ src/cn/msb/java/TestGit.java
@@ -0,0 +1,11 @@
+package cn.msb.java;
+
+/**
+ * @Author:张海峰
+ * @Date：2020/11/14 16:50
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("It's my first commit to github!");
+    }
+}
